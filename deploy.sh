24c01b529a430e97fa20b11b8dd42ed99f545a37
cmd-cv <gaofengim@live.com>
2021-10-29 16:14:52 +0800

    feat: add workflow

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# 发生任何错误时终止
 set -e
 
-# 生成静态文件
+# 构建
 npm run build
 
-# 进入生成的文件夹
-cd /dist
+# 进入输出产物文件夹
+cd dist
 
-# 如果是发布到自定义域名
+# 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
-git add .
+git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git remote add origin git@github.com:cmd-cv/newtab.git
-git branch -M main
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:cmd-cv/newtab.git main:gh-pages
 
 cd -
